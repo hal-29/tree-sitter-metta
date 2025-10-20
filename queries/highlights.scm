2bdf3b38ddcaaefb01489a23127480d2231442ac
hal-29 <haileiyesus29@gmail.com>
2025-10-20 13:37:55 +0300

    update highlight rules

--- queries/highlights.scm
+++ queries/highlights.scm
@@ -1,15 +1,24 @@
-;; Main constructs
+;; Comments
 (comment) @comment
-(string_literal) @string
-(number_literal) @number
-(boolean_literal) @boolean
-(keyword) @keyword
-(operator) @operator
-(variable) @variable
+
+;; Literals inside atoms
+(atom (string_literal) @string)
+(atom (number_literal) @number)
+(atom (boolean_literal) @boolean)
+(atom (char_literal) @character)
+(atom (variable) @variable)
+(atom (symbol) @identifier)
+
+;; Keywords and operators
+(atom (keyword) @keyword)
+(atom (operator) @operator)
+(atom (type_operator) @type)
+(expression (atom (keyword)) @keyword.function)
 
 ;; Punctuation
-(expression ("," @punctuation.delimiter))
-(expression ("(" @punctuation.bracket) (")" @punctuation.bracket))
+("(" @punctuation.bracket)
+(")" @punctuation.bracket)
+("," @punctuation.delimiter)
 
-;; Functions
+;; Function-like symbols in expressions
 (expression (atom (symbol) @function))
